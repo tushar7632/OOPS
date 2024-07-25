ed5efcd25a83fd514b1e0ba50991b13233ff9d94
Tushar <singhtushar763200gmail.com>
2024-07-25 15:37:31 +0530

    this is my201 commit

--- oops1.py
+++ oops1.py
@@ -820,3 +820,25 @@
 # acc1.debit(1000)
 # acc1.credit(500)
 
+
+class Account:
+        def _init_(self, balance,acc):
+                self.balance = balance
+                self.account_no = acc
+                
+                # debit method
+                def debit(self, amount):
+                        self.balance -= amount
+                        print("Rs.", amount, "was debited")
+                        print("total balance = ", self.get_balance())
+                        
+                        def credit(self, credit):
+                                self.balance += amount
+                        print("Rs.", amount, "was credited")
+                        print("total balance = ", self.get_balance())
+        def get__balance(self):
+                return self.balance
+                        
+acc1 = Account(5000, 1000)
+acc1.debit(1000)
+acc1.credit(500)
